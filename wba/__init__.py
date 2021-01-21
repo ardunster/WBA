fb38a5ab64d330e8303c0bca6af71c68cf245658
Anna Dunster <llama747@gmail.com>
2021-01-21 15:51:36 -0800

    Notes and pyside experimentation

---
+++ wba/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  1 14:12:04 2020
+
+@author: adunster
+"""
+
+
--- wba/__main__.py
+++ wba/__main__.py
@@ -15,8 +15,26 @@ from PySide2 import QtCore, QtWidgets, QtGui
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
-        super().__init__(self)
+        super().__init__()
+        print(super())
         self.setWindowTitle("WorldBuilder Alpha")
+        
+        # Menu
+        self.menu = self.menuBar()
+        self.file_menu = self.menu.addMenu("File")
+        
+        # Exit QAction
+        exit_action = QtWidgets.QAction("Exit", self)
+        exit_action.setShortcut(QtGui.QKeySequence.Quit)
+        exit_action.triggered.connect(self.close)
+        
+        self.file_menu.addAction(exit_action)
+        
+        # Status Bar
+        self.status = self.statusBar()
+        self.status.showMessage("Stuff and things")
+        
+        
 
 class Character(QtWidgets.QWidget):
     """
@@ -29,8 +47,11 @@ if __name__ == '__main__':
     # Create the Qt Application
     app = QtWidgets.QApplication(sys.argv)
     # Create and show the form
-    form = Form()
-    form.show()
+    # form = Form()
+    main = MainWindow()
+    main.show()
+    # form.show()
     # Run the main Qt loop
     sys.exit(app.exec_())
-    
+    print('Successfully run __main__.py')
+
